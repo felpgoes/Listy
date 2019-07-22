1f548db4d9c7ae9e328cbc3a34511d5e2643568e
felpgoes <felipe.goess@hotmail.com>
2019-07-21 21:41:37 -0300

    update global archives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListComponent
+    ListComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
