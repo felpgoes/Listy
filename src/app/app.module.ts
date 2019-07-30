6b3e5a07be5dce02a3b32e53e0377f235b23f65a
felpgoes <felipe@devmagic.com.br>
2019-07-30 10:58:30 -0300

    Update path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListComponent } from './list/list.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HowUseComponent } from './how-use/how-use.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,30 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     ListComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    HowUseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { 
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path:'how-use',
+        component: HowUseComponent
+      },
+      {
+        path: 'list',
+        component: ListComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
